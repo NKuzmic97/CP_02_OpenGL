58c47c4708bc2dbecebae7274fcdebfe3c16e51f
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-10-05 22:37:41 +0200

    Vertex attributes and layouts.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -31,9 +31,15 @@ int main(void) {
 		 0.0f,  0.5f,
 		 0.5f, -0.5f
 	};
+
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
