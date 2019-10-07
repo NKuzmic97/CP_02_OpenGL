fa793bccaa211024615652189b9ee9269b20dbe0
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-10-07 16:29:10 +0200

    Writing simple vertex and fragment shader. We get green triangle as output.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -2,6 +2,45 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type,const std::string& source) {
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	
+	if (result == GL_FALSE) {
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!";
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, fs);
+	glAttachShader(program, vs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(fs);
+	glDeleteShader(vs);
+
+	return program;
+}
+
 int main(void) {
 	GLFWwindow* window;
 
@@ -39,7 +78,26 @@ int main(void) {
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	std::string vertexShader = 
+	R"glsl(
+		#version 330 core
+		layout(location = 0) in vec4 position;
+		void main(){
+		gl_Position = position;
+		}
+	)glsl";
+
+	std::string fragmentShader =
+	R"glsl(
+		#version 330 core
+		layout(location = 0) out vec4 color;
+		void main(){
+		color = vec4(0.0, 1.0, 0.0, 1.0);
+		}
+	)glsl";
+
+	unsigned int shader = CreateShader(vertexShader,fragmentShader);
+	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
@@ -48,14 +106,14 @@ int main(void) {
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-
-
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
