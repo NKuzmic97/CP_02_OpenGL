85b062a11f326b28fcb7add181b8c8bb97e34ea1
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-10-07 16:53:08 +0200

    Extracting shaders to Basic.shader file.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,6 +1,40 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource {
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath) {
+	enum class ShaderType {
+		NONE = -1,VERTEX = 0,FRAGMENT = 1
+	};
+
+	std::ifstream stream(filepath);
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+
+	while (getline(stream, line)) {
+		if (line.find("#shader") != std::string::npos) {
+			if (line.find("vertex") != std::string::npos) {
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos) {
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else {
+			ss[(int)type] << line << '\n';
+		}
+	}
+	return { ss[0].str(),ss[1].str() };
+}
 
 static unsigned int CompileShader(unsigned int type,const std::string& source) {
 	unsigned int id = glCreateShader(type);
@@ -78,25 +112,9 @@ int main(void) {
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-	std::string vertexShader = 
-	R"glsl(
-		#version 330 core
-		layout(location = 0) in vec4 position;
-		void main(){
-		gl_Position = position;
-		}
-	)glsl";
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
-	std::string fragmentShader =
-	R"glsl(
-		#version 330 core
-		layout(location = 0) out vec4 color;
-		void main(){
-		color = vec4(0.0, 1.0, 0.0, 1.0);
-		}
-	)glsl";
-
-	unsigned int shader = CreateShader(vertexShader,fragmentShader);
+	unsigned int shader = CreateShader(source.VertexSource,source.FragmentSource);
 	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
@@ -113,7 +131,7 @@ int main(void) {
 		glfwPollEvents();
 	}
 
-	glDeleteProgram(shader);
+	//glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
