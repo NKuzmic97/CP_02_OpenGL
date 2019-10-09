523203eedcdb635e5a98107c2a95a4b4265e9a9d
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-10-09 16:27:50 +0200

    Basic renderer class in OpenGL.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -7,6 +7,7 @@
 
 #include "Renderer.h"
 #include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
@@ -67,6 +68,7 @@ int main(void) {
 		vb.Unbind();
 		ib.Unbind();
 
+		Renderer renderer;
 		float r = 0.0f;
 		float increment = 0.05f;
 
@@ -74,15 +76,12 @@ int main(void) {
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window)) {
 			/* Render here */
-			GLCall(glClear(GL_COLOR_BUFFER_BIT));
+			renderer.Clear();
 
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
-			va.Bind();
-			ib.Bind();
-
-			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+			renderer.Draw(va, ib, shader);
 
 			if (r > 1.0f) increment = -0.05f;
 			else  if (r < 0.0f) increment = 0.05f;
