55e05bb16019d5a48a7601b653b152cc41140e2f
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-10-09 18:28:22 +0200

    Using textures in OpenGL.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -11,6 +11,7 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 int main(void) {
 	GLFWwindow* window;
@@ -42,10 +43,10 @@ int main(void) {
 
 	{
 		float positions[] = {
-			 -0.5f, -0.5f,
-			  0.5f,  -0.5f,
-			  0.5f, 0.5f,
-			 -0.5f, 0.5f,
+			 -0.5f, -0.5f, 0.0f, 0.0f,
+			  0.5f, -0.5f, 1.0f, 0.0f,
+			  0.5f,  0.5f, 1.0f, 1.0f,
+			 -0.5f,  0.5f, 0.0f, 1.0f
 		};
 
 		unsigned int indices[] = {
@@ -53,20 +54,32 @@ int main(void) {
 			2,3,0
 		};
 
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+		GLCall(glEnable(GL_BLEND));
+
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+
 		VertexBufferLayout layout;
 		layout.Push<float>(2);
+		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
+
 		IndexBuffer ib(indices, 6);
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+		Texture texture("res/textures/texture.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
+
 		va.Unbind();
-		shader.Unbind();
 		vb.Unbind();
 		ib.Unbind();
+		shader.Unbind();
+
 
 		Renderer renderer;
 		float r = 0.0f;
@@ -80,7 +93,6 @@ int main(void) {
 
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-
 			renderer.Draw(va, ib, shader);
 
 			if (r > 1.0f) increment = -0.05f;
