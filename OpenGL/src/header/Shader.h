e66a003dcd9f1af6a522b9cee6692f7ea090186d
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-10-22 21:32:52 +0200

    How to cache uniforms in OpenGL using unordered map

--- OpenGL/src/header/Shader.h
+++ OpenGL/src/header/Shader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include <GL/glew.h>
 
 #include "glm/glm.hpp"
 
@@ -13,7 +14,7 @@ class Shader {
 private:
 	std::string m_FilePath;
 	unsigned int m_RendererID;
-	std::unordered_map<std::string, int> m_UniformLocationCache;
+	mutable std::unordered_map<std::string, GLint> m_UniformLocationCache;
 	// caching for uniforms
 public:
 	Shader(const std::string& filepath);
@@ -29,8 +30,8 @@ public:
 	void SetUniformMat4f(const std::string& name,const glm::mat4& matrix);
 
 private:
-	int GetUniformLocation(const std::string& name);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 	ShaderProgramSource ParseShader(const std::string& filepath);
+	int GetUniformLocation(const std::string& name) const;
 };
